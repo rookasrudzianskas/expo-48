fdae4f55fc6e3ca2a2020b25a6c0652677f10540
Rokas <rookas.rudzenskas@gmail.com>
2023-02-28 18:12:52 +0200

    feat: add three.tsx
    - Add three.tsx
    - Add three.tsx
    - Add three.tsx

---
+++ app/(tabs)/three.tsx
@@ -0,0 +1,15 @@
+//@ts-nocheck
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const Three = () => {
+  return (
+    <View>
+      <Text>
+        byrookas 🚀 - Three
+      </Text>
+    </View>
+  );
+};
+
+export default Three;
